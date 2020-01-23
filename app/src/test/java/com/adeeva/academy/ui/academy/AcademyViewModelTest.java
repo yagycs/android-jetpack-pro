e621ab9d536fc62e78cf0c1cee5c9618501c3ef2
yagycs <yagybatman@gmail.com>
2020-01-23 18:29:27 +0700

    updated

---
+++ app/src/test/java/com/adeeva/academy/ui/academy/AcademyViewModelTest.java
@@ -0,0 +1,26 @@
+package com.adeeva.academy.ui.academy;
+
+import com.adeeva.academy.data.CourseEntity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class AcademyViewModelTest {
+    private AcademyViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new AcademyViewModel();
+    }
+
+    @Test
+    public void getCourses() {
+        List<CourseEntity> courseEntities = viewModel.getCourses();
+        assertNotNull(courseEntities);
+        assertEquals(5, courseEntities.size());
+    }
+}
---
+++ app/src/test/java/com/adeeva/academy/ui/bookmark/BookmarkViewModelTest.java
@@ -0,0 +1,26 @@
+package com.adeeva.academy.ui.bookmark;
+
+import com.adeeva.academy.data.CourseEntity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class BookmarkViewModelTest {
+    private BookmarkViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new BookmarkViewModel();
+    }
+
+    @Test
+    public void getBookmark() {
+        List<CourseEntity> courseEntities = viewModel.getBookmarks();
+        assertNotNull(courseEntities);
+        assertEquals(5, courseEntities.size());
+    }
+}
